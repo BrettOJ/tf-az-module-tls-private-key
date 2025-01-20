cd6e7cbaf37319fda94404b31de9994d6e003e8b
Brett Odgers-Jewell <brett.odgers-jewell@konjur.sg>
2025-01-20 10:32:08 +0800

    update module

--- module.tf
+++ module.tf
@@ -9,7 +9,7 @@ resource "azurerm_ssh_public_key" "ssh_pk" {
   name                = module.sshpk_name.naming_convention_output[var.naming_convention_info.name].names.0
   resource_group_name = var.resource_group_name
   location            = var.location
-  public_key          = each.value
+  public_key          = each.value["public_key_openssh"]
   depends_on          = [tls_private_key.prvt_key]
 }
 
