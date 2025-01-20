8660e665359d503146425b505fdcb715c4914a1b
Brett Odgers-Jewell <brett.odgers-jewell@konjur.sg>
2025-01-20 13:03:05 +0800

    update module

--- ssh-pk.tf
+++ ssh-pk.tf
@@ -3,10 +3,10 @@ locals {
 }
 
 resource "azurerm_ssh_public_key" "ssh_pk" {
-  for_each            = var.create_ssh_public_key ? { for key, value in tls_private_key.prvt_key : key => {public_key = lookup(value,public_key_openssh)} } : {}
-  name                = module.sshpk_name.naming_convention_output[var.naming_convention_info.name].names.0
+  for_each            = var.create_ssh_public_key ? { public_key = tls_private_key.prvt_key.public_key_openssh } : {}
+  name                = module.sshpk_name.naming_convention_output[var.naming_convention_info.name].names[0]
   resource_group_name = var.resource_group_name
   location            = var.location
-  public_key          = each.value.public_key
+  public_key          = each.value
   depends_on          = [tls_private_key.prvt_key]
 }
