c219e79689e11bf08f8f32899ceb8769474ae7af
Brett Odgers-Jewell <brett.odgers-jewell@konjur.sg>
2025-01-20 12:50:41 +0800

    update module

--- module.tf
+++ module.tf
@@ -4,12 +4,3 @@ resource "tls_private_key" "prvt_key" {
   ecdsa_curve = var.ecdsa_curve
 }
 
-resource "azurerm_ssh_public_key" "ssh_pk" {
-  for_each            = var.create_ssh_public_key ? { for k, v in tls_private_key.prvt_key : k => v.public_key_openssh } : {}
-  name                = module.sshpk_name.naming_convention_output[var.naming_convention_info.name].names.0
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  public_key          = each.value
-  depends_on          = [tls_private_key.prvt_key]
-}
-
---
+++ ssh-pk.tf
@@ -0,0 +1,12 @@
+locals {
+  
+}
+
+resource "azurerm_ssh_public_key" "ssh_pk" {
+  for_each            = var.create_ssh_public_key ? { for key, value in tls_private_key.prvt_key : key => {public_key = lookup(value,public_key_openssh)} } : {}
+  name                = module.sshpk_name.naming_convention_output[var.naming_convention_info.name].names.0
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  public_key          = each.value.public_key
+  depends_on          = [tls_private_key.prvt_key]
+}
